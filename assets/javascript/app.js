a514f209df03b3af4a0d1c1c83c514002d342e98
Ken Riley <kriley314@gmail.com>
2019-03-12 14:41:35 -0500

    Closest?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,37 +13,95 @@ window.onload=function() {
     $(".theQuestions").hide();
     $(".theScore").hide();
     $("#btn-stop").hide();
+    $("#timerDisplay").hide();
 }
 
+var nTestTime = 30;  // Allow 90 seconds..
 var testTimeout;
+var intervalId;
+
+function runTimer() {
+    clearInterval(intervalId);
+    intervalId = setInterval( decrement, 1000 );
+  }
+
 
 $("#btn-start").on("click", function() {
     // Call the function that displays the test questions..
     $(".theQuestions").show();
+    $("#timerDisplay").show();
 
     // Hide the start button and show the stop button..
     $("#btn-start").hide();
     $("#btn-stop").show();
 
-    // Start the game timer..  45 seconds should be pleny of time..
-    testTimeout = setTimeout( timeUp, 1000 * 45 );
+    // Start the game timer se defined by nTestTime - in seconds..
+    testTimeout = setTimeout( timeUp, 1000 * nTestTime );
+    runTimer();
 });
 
+function timeConverter(t) {
+
+var minutes = Math.floor(t / 60);
+var seconds = t - (minutes * 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes === 0) {
+    minutes = "00";
+  }
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  
+  return minutes + ":" + seconds;
+}
+  
+//  The decrement function.
+function decrement() {
+
+    //  Decrease number by one.
+    nTestTime--;
+
+    $("#timerDisplay").html("<h2>" + timeConverter( nTestTime ) + "</h2>");
+
+    //  Once number hits zero...
+    if (nTestTime === 0) {
+      //  ...run the stop function.
+      stop();
+      clearInterval(intervalId);
+
+      // End the test..
+      timeUp();
+    }
+}
+  
+  
 function timeUp() {
     // in the element with an id of time-left add an h2 saying Time's Up!
     // console log done
     console.log("Time ran out");
+    clearInterval(intervalId);
+
+    // Call end of test method to calculate score and so on..
     endTest();
 }
   
   
 $("#btn-stop").on("click", function() {
     // Stop the timer..
-    clearTimeout( testTimeout );
+    stop();
+    clearInterval(intervalId);
 
     // Call end of test method to calculate score and so on..
     endTest();
 });
+
+var nRight = 0;
+var nWrong = 0;
+var nUnanswered = 0;
 
 function endTest() {
     // Hide the test itself..  Hide the stop button..
@@ -54,10 +112,6 @@ function endTest() {
     $(".theScore").show();
 
     // Calculate the scores..
-    var nRight = 0;
-    var nWrong = 0;
-    var nUnanswered = 0;
-    
     var strTheirAnswer;
     var radios = document.getElementsByName( "collide" );
     for (var i = 0; i < radios.length; i++){
@@ -164,7 +218,6 @@ function endTest() {
       nWrong++;
     }
 
-    debugger;
     radios = document.getElementsByName( "dense" );
     for (var i = 0; i < radios.length; i++){
       if ( radios[i].checked ) {
@@ -196,10 +249,9 @@ function endTest() {
     }
 
     // Now we have the stats..  Update the screen already!!
-    debugger;
-    $("correctAnswers").text( "Correct Answers: " + nRight );
-    $("#wrongAnswers").text( "Wrong Answers: " + nWrong );
-    $("#unAnswered").value( "Unanswered: " + nUnanswered );
+    $("#numRight").text( nRight );
+    $("#numWrong").text( nWrong );
+    $("#numUnAnswered").text( nUnanswered );
 
     $(".theScore").show();
 }
